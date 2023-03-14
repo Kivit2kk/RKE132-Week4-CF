0f449ae596071fe6a79c23d867295533b77945d2
kivit2kk <martinkivi93@gmail.com>
2023-03-14 19:28:37 +0200

    add week 4 code

---
+++ Nädal4Test/Program.cs
@@ -0,0 +1,19 @@
+﻿string userPin;
+
+int triesUsed = 0;
+
+
+while (triesUsed != 3)
+{
+    Console.WriteLine("Enter your PIN.");
+    userPin= Console.ReadLine();
+    if (userPin == "1234") 
+    {
+        Console.WriteLine("Welcome!");
+    }
+    else 
+    {
+        Console.WriteLine($"Wrong PIN Try again.{3-triesUsed} tries left.");
+        triesUsed++;
+    }
+}
